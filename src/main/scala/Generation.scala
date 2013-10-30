cf903a858fbe2dfed68fc34d0753f7ebf92d1b91
Heiko Seeberger <heiko.seeberger@gmail.com>
2013-10-30 13:59:02 +0100

    Provide proper implementation for `next` method of `Generation`

--- src/main/scala/Generation.scala
+++ src/main/scala/Generation.scala
@@ -1,5 +1,23 @@
 case class Generation(aliveCells: Set[Cell] = Set.empty) {
 
   def next: Generation =
-    Generation()
+    new Generation(stayingAlive ++ born)
+
+  def stayingAlive: Set[Cell] =
+    aliveCells filter canStayAlive
+
+  def born: Set[Cell] =
+    aliveCells flatMap deadNeighbors filter canBeBorn
+
+  def canStayAlive(cell: Cell): Boolean =
+    Set(2, 3) contains aliveNeighbors(cell).size
+
+  def canBeBorn(cell: Cell): Boolean =
+    aliveNeighbors(cell).size == 3
+
+  def aliveNeighbors(cell: Cell): Set[Cell] =
+    cell.neighbors intersect aliveCells
+
+  def deadNeighbors(cell: Cell): Set[Cell] =
+    cell.neighbors diff aliveCells
 }
